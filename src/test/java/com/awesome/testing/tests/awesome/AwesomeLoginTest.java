7163e2def88f7b1759e1ea3916b070139860dcf9
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-06-03 08:38:21 +0200

    parametrised test

--- src/test/java/com/awesome/testing/tests/awesome/AwesomeLoginTest.java
+++ src/test/java/com/awesome/testing/tests/awesome/AwesomeLoginTest.java
@@ -2,15 +2,24 @@ package com.awesome.testing.tests.awesome;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
 public class AwesomeLoginTest extends AbstractAwesomeTest {
 
+    // https://www.baeldung.com/parameterized-tests-junit-5
     @ParameterizedTest
     @MethodSource("credentials")
     public void shouldLogin(String username, String password) {
+        awesomeLoginPage.attemptLogin(username, password)
+                .verifyUsersDisplayed();
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1)
+    public void shouldLoginWithCsvSource(String username, String password) {
         awesomeLoginPage.attemptLogin(username, password)
                 .verifyUsersDisplayed();
     }
