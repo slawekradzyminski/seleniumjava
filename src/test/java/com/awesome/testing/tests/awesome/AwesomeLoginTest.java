3334e1565dbe6ce65648664ffca287ac2ba12b34
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-11-21 11:46:15 +0100

    nov03: @ParametrizedTest with static test data and @MethodSource

--- src/test/java/com/awesome/testing/tests/awesome/AwesomeLoginTest.java
+++ src/test/java/com/awesome/testing/tests/awesome/AwesomeLoginTest.java
@@ -1,13 +1,25 @@
 package com.awesome.testing.tests.awesome;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class AwesomeLoginTest extends AbstractAwesomeTest {
 
-    @Test
-    public void shouldSuccessfullyLogin() {
-        loginPage.attemptLogin("admin", "admin")
-                .verifyHeaderForName("Slawomir");
+    @ParameterizedTest
+    @MethodSource("testData")
+    public void shouldSuccessfullyLogin(String username, String password, String firstName) {
+        loginPage.attemptLogin(username, password)
+                .verifyHeaderForName(firstName);
+    }
+
+    public static Stream<Arguments> testData() {
+        return Stream.of(
+          Arguments.of("admin", "admin", "Slawomir"),
+          Arguments.of("client", "client", "Gosia")
+        );
     }
 
 }
