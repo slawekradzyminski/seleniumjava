4986493419c6b863c9cd13377e2304650a6d64e4
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-11-21 10:25:08 +0100

    nov01

--- src/test/java/com/awesome/testing/tests/SeleniumTest.java
+++ src/test/java/com/awesome/testing/tests/SeleniumTest.java
@@ -1,15 +1,20 @@
 package com.awesome.testing.tests;
 
+import com.awesome.testing.properties.TestProperties;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public abstract class SeleniumTest {
 
-    protected ChromeDriver driver;
+    protected WebDriver driver;
+    protected TestProperties testProperties;
 
     @BeforeAll
     static void setupDriver() {
@@ -17,13 +22,25 @@ public abstract class SeleniumTest {
     }
 
     @BeforeEach
-    public void setUpDriver() {
-        driver = new ChromeDriver();
+    public void setUpTest() {
+        testProperties = new TestProperties();
+        driver = getBrowser();
+    }
+
+    private WebDriver getBrowser() {
+        return switch (testProperties.getBrowser()) {
+            case "chrome" -> new ChromeDriver();
+            case "edge" -> new EdgeDriver();
+            case "firefox" -> new FirefoxDriver();
+            default -> throw new IllegalStateException("Unexpected value: " + testProperties.getBrowser());
+        };
     }
 
     @AfterEach
     void teardown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
 }
