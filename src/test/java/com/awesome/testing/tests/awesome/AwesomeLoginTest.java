42b2f09eceae586429c925aa060749a23cf7c76f
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-04-24 12:55:27 +0200

    stu07

--- src/test/java/com/awesome/testing/tests/awesome/AwesomeLoginTest.java
+++ src/test/java/com/awesome/testing/tests/awesome/AwesomeLoginTest.java
@@ -2,25 +2,15 @@ package com.awesome.testing.tests.awesome;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class AwesomeLoginTest extends AbstractAwesomeTest {
 
     @ParameterizedTest
-    @MethodSource("testData")
+    @CsvFileSource(resources = "/testdata.csv", numLinesToSkip = 1)
     public void shouldSuccessfullyLogin(String username, String password, String firstName) {
         awesomeLoginPage.attemptLogin(username, password)
                 .assertThatHeaderContains(firstName);
-    }
-
-    public static Stream<Arguments> testData() {
-        return Stream.of(
-                Arguments.of("admin", "admin", "Slawomir"),
-                Arguments.of("client", "client", "Gosia")
-        );
     }
 
     @Test
