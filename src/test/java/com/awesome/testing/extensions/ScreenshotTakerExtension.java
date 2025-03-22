4464fd68e9202bb5654903161dbc122e6111736f
slawekradzyminski <slawomir.radzyminski@gmail.com>
2025-03-22 11:57:25 +0100

    Attach to Allure

--- src/test/java/com/awesome/testing/extensions/ScreenshotTakerExtension.java
+++ src/test/java/com/awesome/testing/extensions/ScreenshotTakerExtension.java
@@ -1,5 +1,6 @@
 package com.awesome.testing.extensions;
 
+import io.qameta.allure.Allure;
 import lombok.Setter;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,8 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
@@ -24,13 +27,26 @@ public class ScreenshotTakerExtension implements AfterTestExecutionCallback {
     @SneakyThrows
     public void afterTestExecution(ExtensionContext extensionContext) {
         if (extensionContext.getExecutionException().isPresent()) {
-            log.info("Detected test failure. Attempt to create a screenshot");
-            String baseFileName = getBaseFileName(extensionContext);
+            // 1. Log
+            log.info("Detected test failure. Attempting to create a screenshot.");
+
+            // 2. Capture screenshot file
             File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+            // 3. Build a filename
+            String baseFileName = getBaseFileName(extensionContext);
             String userDirectory = System.getProperty("user.dir");
             File target = new File(userDirectory + "/target/" + baseFileName + ".png");
+
+            // 4. Copy the screenshot file to target/
             Files.copy(screenshot.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
-            log.info("Screenshot created in {}", target);
+            log.info("Screenshot saved to {}", target.getAbsolutePath());
+
+            // 5. Attach to Allure
+            //    We must read the file bytes and pass them to Allure.addAttachment
+            try (InputStream is = new FileInputStream(target)) {
+                Allure.addAttachment("Failure Screenshot [" + baseFileName + "]", "image/png", is, "png");
+            }
         }
     }
 
@@ -39,5 +55,4 @@ public class ScreenshotTakerExtension implements AfterTestExecutionCallback {
                 + context.getRequiredTestMethod().getName() + "-"
                 + LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyy-HHmmss"));
     }
-
 }
