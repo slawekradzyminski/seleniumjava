8349d229b5d6a526da7c2b01e08af8e6943508da
slawekradzyminski <slawomir.radzyminski@gmail.com>
2025-03-28 11:24:22 +0100

    list15

---
+++ src/test/java/com/awesome/testing/tests/AwesomeTestSuite.java
@@ -0,0 +1,10 @@
+package com.awesome.testing.tests;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("com.awesome.testing.tests.awesome")
+public class AwesomeTestSuite {
+
+}
