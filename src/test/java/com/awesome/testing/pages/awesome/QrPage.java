2693777e86f5dc44983ebd8421b6535b70d5d592
slawekradzyminski <slawomir.radzyminski@gmail.com>
2025-03-28 10:39:26 +0100

    list14

--- src/test/java/com/awesome/testing/pages/awesome/QrPage.java
+++ src/test/java/com/awesome/testing/pages/awesome/QrPage.java
@@ -1,11 +1,15 @@
 package com.awesome.testing.pages.awesome;
 
 import com.awesome.testing.pages.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static org.awaitility.Awaitility.await;
 
 public class QrPage extends BasePage {
+
+    private static final By QR_CODE = By.cssSelector("[alt='Generated QR Code']");
 
     public QrPage(WebDriver driver) {
         super(driver);
@@ -15,5 +19,26 @@ public class QrPage extends BasePage {
     public void verifyIsLoaded() {
         await().ignoreException(NullPointerException.class)
                 .until(() -> driver.getPageSource().contains("QR Code Generator"));
+    }
+
+    public QrPage generateQr(String text) {
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("qr-text")));
+        driver.findElement(By.id("qr-text")).sendKeys(text);
+        driver.findElement(By.xpath("//button[text()='Generate QR Code']")).click();
+        return this;
+    }
+
+    public QrPage assertQrCodeDisplayed() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(QR_CODE));
+        return this;
+    }
+
+    public QrPage clearQrCode() {
+        driver.findElement(By.xpath("//button[text()='Clear']")).click();
+        return this;
+    }
+
+    public void assertQrCodeNotDisplayed() {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(QR_CODE));
     }
 }
--- src/test/java/com/awesome/testing/tests/awesome/QrTest.java
+++ src/test/java/com/awesome/testing/tests/awesome/QrTest.java
@@ -1,13 +1,24 @@
 package com.awesome.testing.tests.awesome;
 
 import com.awesome.testing.config.ConfigProvider;
+import com.awesome.testing.pages.awesome.QrPage;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class QrTest extends LoggedInSeleniumTest {
 
     @BeforeEach
     public void setUp() {
         driver.navigate().to(ConfigProvider.get("frontend.url") + "/qr");
+    }
+
+    @Test
+    public void shouldGenerateQrCodeAndClearItSuccessfully() {
+        new QrPage(driver)
+                .generateQr("https://www.awesome-testing.com")
+                .assertQrCodeDisplayed()
+                .clearQrCode()
+                .assertQrCodeNotDisplayed();
     }
 
 }
